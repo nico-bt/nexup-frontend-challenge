aab5311407a587c1e8227e77ea7d8d7df7ac5d5b
Nico-Battaglia <nico-battaglia@hotmail.com>
2026-03-03 08:43:38 -0300

    Refactor CategoryFilter and ProductManager components to implement category selection and product filtering functionality

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { CategoryFilterType, ProductCategory } from '../models/ProductCategory';
 
-interface CategoryFilterProps {}
+type Props = {
+  selectedCategory: CategoryFilterType;
+  onChangeCategory: (category: CategoryFilterType) => void;
+};
 
-export const CategoryFilter: React.FC<CategoryFilterProps> = () => {
-  return <div />;
+export const CategoryFilter: React.FC<Props> = ({
+  selectedCategory,
+  onChangeCategory,
+}) => {
+  const categories = Object.values(ProductCategory);
+
+  return (
+    <div>
+      <div>
+        <span>Filter by: </span>
+        <select
+          value={selectedCategory}
+          onChange={(e) =>
+            onChangeCategory(e.target.value as CategoryFilterType)
+          }
+        >
+          <option value="All">All</option>
+
+          {categories.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 };
--- src/components/ProductManager.tsx
+++ src/components/ProductManager.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import { ProductList } from './ProductList';
+import React, { useEffect, useMemo, useState } from 'react';
+import { ProductList } from './ProductsList/ProductList';
+import { Product } from '../models/Product';
+import { getProductList } from '../api/products';
+import { CategoryFilterType } from '../models/ProductCategory';
 import { CategoryFilter } from './CategoryFilter';
 
 export const ProductManager: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [category, setCategory] = useState<CategoryFilterType>('All');
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setError(null);
+      setIsLoading(true);
+      try {
+        const data = await getProductList();
+        setProducts(data);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : 'Something went wrong');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getProducts();
+  }, []);
+
+  const filteredProducts = useMemo(() => {
+    if (category === 'All') return products;
+
+    return products.filter((product) => product.category === category);
+  }, [products, category]);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  if (products.length === 0) return <p>No items</p>;
+
   return (
     <div>
-      <CategoryFilter />
-      <ProductList productList={[]} />
+      <CategoryFilter
+        selectedCategory={category}
+        onChangeCategory={setCategory}
+      />
+      <ProductList productList={filteredProducts} />
     </div>
   );
 };
